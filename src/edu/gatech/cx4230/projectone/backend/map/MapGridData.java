5a6b670a4e69e945950c8f10ea09734c8b7f51ab
mcdc09 <ThomasBowling@Comcast.net>
2014-01-22 11:17:34 -0500

    Created MapGridData class

---
+++ src/edu/gatech/cx4230/projectone/backend/map/MapGridData.java
@@ -0,0 +1,13 @@
+package edu.gatech.cx4230.projectone.backend.map;
+
+/**
+ * Class holds information for the map grid (buildings, sidewalks, cross walks, streets, etc.)
+ * @author tbowling3
+ *
+ */
+public class MapGridData {
+
+	public MapGridData() {
+		
+	}
+}
