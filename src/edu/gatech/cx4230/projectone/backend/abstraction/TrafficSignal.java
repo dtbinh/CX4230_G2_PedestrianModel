9b66f342cbbab40c36364264fba3a40e2d17c14a
mcdc09 <ThomasBowling@Comcast.net>
2014-02-02 14:48:40 -0600

    Created a TrafficSignal object

---
+++ src/edu/gatech/cx4230/projectone/backend/abstraction/TrafficSignal.java
@@ -0,0 +1,19 @@
+package edu.gatech.cx4230.projectone.backend.abstraction;
+
+/**
+ * Object that represents a road intersection which involves cross-walks.  This
+ * is a secondary goal for the project.
+ * @author tbowling3
+ *
+ */
+public class TrafficSignal {
+	
+	public TrafficSignal() {
+		
+	}
+	
+	public void timeStep(int timeStep) {
+		
+	}
+
+}
