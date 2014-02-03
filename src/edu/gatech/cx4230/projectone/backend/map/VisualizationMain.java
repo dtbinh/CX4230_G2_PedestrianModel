d6f7ecb975447a80b1f199bfdf0343c8006360d8
mcdc09 <ThomasBowling@Comcast.net>
2014-02-03 17:28:55 -0500

    Added markers for People

--- src/edu/gatech/cx4230/projectone/backend/map/VisualizationMain.java
+++ src/edu/gatech/cx4230/projectone/backend/map/VisualizationMain.java
@@ -1,5 +1,6 @@
 package edu.gatech.cx4230.projectone.backend.map;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import processing.core.PApplet;
@@ -7,8 +8,10 @@ import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.utils.MapUtils;
+import edu.gatech.cx4230.projectone.backend.abstraction.Cell;
 import edu.gatech.cx4230.projectone.backend.abstraction.Person;
 import edu.gatech.cx4230.projectone.backend.main.PedestrianSimulation;
+import edu.gatech.cx4230.projectone.visualization.abstraction.PersonMarker;
 
 /**
  * Object that will be the main driver for the visualization of the simulation
@@ -21,7 +24,7 @@ public class VisualizationMain extends PApplet {
 	 * 
 	 */
 	private static final long serialVersionUID = -1551910428456184440L;
-	
+
 	public static final double CELL_FACTOR_X = 0.05, CELL_FACTOR_Y = 0.04;
 	private UnfoldingMap map;
 	private static final int WIDTH = 1000, HEIGHT = 800;
@@ -30,56 +33,71 @@ public class VisualizationMain extends PApplet {
 	private PedestrianSimulation ps;
 	private List<Marker> markers;
 	private List<Marker> peopleMarkers;
-	
-	
+
+
 	public void setup() {
 		size(WIDTH, HEIGHT);
-		
+
 		map = new UnfoldingMap(this);
 
 		MapUtils.createDefaultEventDispatcher(this, map);
 		map.zoomAndPanTo(new Location(TOP_LEFT_LAT, TOP_LEFT_LON), 5);
-		
-		//noLoop();
-		
-		ps = new PedestrianSimulation();
-		ps.mainLoop(this);
-		
-		redraw();
-		
+		peopleMarkers = new ArrayList<Marker>();
+
+		ps = new PedestrianSimulation(this);
 	}
-	
-	public void updatePeopleMarkers(List<Person> people) {
-		
-	}
-	
+
+
 	/**
 	 * Draw operation called by the PApplet
 	 */
 	public void draw() {
 		background(175);
 		map.updateMap();
-		
+
 		if(markers != null && !markers.isEmpty()) {
 			for(Marker m: markers) {
 				m.draw(map);
 			}
 		}
-		
+
+		updatePeopleMarkers();
 		if(peopleMarkers != null && !peopleMarkers.isEmpty()) {
 			for(Marker m: peopleMarkers) {
 				m.draw(map);
 			}
 		}
-	}
-	
+	} // close draw
+
+	private void updatePeopleMarkers() {
+		if(ps.peopleAvailable()) {
+			List<Person> people = ps.getPeople();
+
+			// TODO Update the list of people markers given a list of persons
+			if(people != null && !people.isEmpty()) {
+				peopleMarkers.clear();
+
+				for(Person p: people) {
+					Cell location = p.getLocation();
+					if(location != null) {
+						double x = location.getX() + 0.5;
+						double y = location.getY() + 0.5;
+						Location loc = getMapLocationForCellLocation(x, y);
+						PersonMarker pm = new PersonMarker(loc, p.getStressLevel());
+						peopleMarkers.add(pm);
+					} // close if
+				} // close people for
+			} // close null if
+		} // close people if
+	} // close updatePeopleMarkers()
+
 	/**
 	 * Returns the map position of the top left corner of a given cell index (x,y)
 	 * @param x
 	 * @param y
 	 * @return
 	 */
-	public Location getMapLocationForCellLocation(int x, int y) {
+	public Location getMapLocationForCellLocation(double x, double y) {
 		double lon = TOP_LEFT_LON + x * CELL_FACTOR_X;
 		double lat = TOP_LEFT_LAT - y * CELL_FACTOR_Y;
 		return new Location(lat, lon);
@@ -112,7 +130,7 @@ public class VisualizationMain extends PApplet {
 	public void setPeopleMarkers(List<Marker> peopleMarkers) {
 		this.peopleMarkers = peopleMarkers;
 	}
-	
+
 	public static void main(String[] args) {
 		PApplet.main(new String[] {"edu.gatech.cx4230.projectone.backend.map.VisualizationMain"});
 	}
--- src/edu/gatech/cx4230/projectone/visualization/abstraction/PersonMarker.java
+++ src/edu/gatech/cx4230/projectone/visualization/abstraction/PersonMarker.java
@@ -1,9 +1,17 @@
 package edu.gatech.cx4230.projectone.visualization.abstraction;
 
+import processing.core.PConstants;
 import processing.core.PGraphics;
+import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 
 public class PersonMarker extends SimplePointMarker {
+	private int colorR, colorG, colorB;
+	
+	public PersonMarker(Location loc, double stress) {
+		super(loc);
+		setColorForStress(stress);
+	}
 
 	/* (non-Javadoc)
 	 * @see de.fhpotsdam.unfolding.marker.SimplePointMarker#draw(processing.core.PGraphics, float, float)
@@ -17,14 +25,19 @@ public class PersonMarker extends SimplePointMarker {
 		pg.pushStyle();
 		pg.strokeWeight(strokeWeight);
 		if (isSelected()) {
-			pg.fill(highlightColor);
-			pg.stroke(highlightStrokeColor);
+			pg.fill(colorR, colorG, colorB);
+			pg.stroke(colorR, colorG, colorB);
 		} else {
-			pg.fill(color);
-			pg.stroke(strokeColor);
+			pg.fill(colorR, colorG, colorB);
+			pg.stroke(colorR, colorG, colorB);
 		}
+		pg.ellipseMode(PConstants.CENTER);
 		pg.ellipse((int) x, (int) y, radius, radius); // TODO use radius in km and convert to px
 		pg.popStyle();
+	}
+	
+	private void setColorForStress(double stress) {
+		
 	}
 	
 	
