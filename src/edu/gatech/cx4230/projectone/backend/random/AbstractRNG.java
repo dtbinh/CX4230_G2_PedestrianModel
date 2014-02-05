72a64854a1ea37a616ad92ce03df576946a86288
mcdc09 <ThomasBowling@Comcast.net>
2014-02-05 11:30:08 -0500

    Added more useful methods to AbstractRNG for ints

--- src/edu/gatech/cx4230/projectone/backend/random/AbstractRNG.java
+++ src/edu/gatech/cx4230/projectone/backend/random/AbstractRNG.java
@@ -103,6 +103,74 @@ public abstract class AbstractRNG {
 		return a;
 	}
 	
+	/**
+	 * Generates the next int in the sequence with a range [min, max)
+	 * @param min
+	 * @param max
+	 * @return the next int in the sequence with a range [min, max)
+	 */
+	public int nextIntInRange(int min, int max) {
+		return min + nextInt() * ((max - min) + 1);
+	}
+	
+	/**
+	 * Returns a sorted array of a specific length of the next ints in the sequence
+	 * @param n the length of the array to be returned
+	 * @return a sorted array of a specific length of the next ints in the sequence
+	 */
+	public int[] nextIntArraySorted(int n) {
+		int[] a = nextIntsArray(n);
+		Arrays.sort(a);
+		
+		return a;
+	}
+	
+	/**
+	 * Returns an array of a specific length of the next ints in the sequence
+	 * @param n the length of the array to be returned
+	 * @return an array of a specific length of the next doubles in the sequence
+	 */
+	public int[] nextIntsArray(int n) {
+		int[] out = new int[n];
+		
+		for(int i = 0; i < out.length; i++) {
+			out[i] = nextInt();
+		}
+		
+		return out;
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public int[] nextIntsArray(int n, int min, int max) {
+		int[] out = new int[n];
+		
+		for(int i = 0; i < out.length; i++) {
+			out[i] = nextIntInRange(min, max);
+		}
+		
+		return out;
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public int[] nextIntsArraySorted(int n, int min, int max) {
+		int[] a = nextIntsArray(n, min, max);
+		Arrays.sort(a);
+		
+		return a;
+	}
+	
 	
 	
 
