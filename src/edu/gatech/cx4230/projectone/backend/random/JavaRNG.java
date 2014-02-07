1a01e635b119468edfa899e2c795c298f9f9af5e
Elliott, Tucker Sterling <tselliott@gatech.edu>
2014-02-06 20:03:27 -0500

    Implemented an actual RNG
    Uses an XORShift, currently returns a random number between 0 and 99.

--- src/edu/gatech/cx4230/projectone/backend/random/JavaRNG.java
+++ src/edu/gatech/cx4230/projectone/backend/random/JavaRNG.java
@@ -3,34 +3,54 @@ package edu.gatech.cx4230.projectone.backend.random;
 import java.util.Random;
 
 /**
- * Random number generator which uses Java's built in Random object.  This
- * object is not for use with the final project, but is instead meant to be
- * used as a test RNG during development.
- * @author tbowling3
+ * RNG using xorshift.
+ * @author telliott8
  *
  */
 public class JavaRNG extends AbstractRNG {
 	private Random rand;
 	
-	public JavaRNG() {
-		rand = new Random();
+	
+	public JavaRNG() {}
+	
+	private long RNG() {
+		long a = System.nanoTime() % 100000;
+		long b = System.nanoTime() % 200000;
+		long c = System.nanoTime() % 400000;
+		long d = System.nanoTime() % 800000;
+		
+		long t = a ^ (a << 11);
+		a = b;
+		b = c;
+		c = d;
+		d = (d ^ (d >>> 19) ^ (t ^ (t >>> 8)));
+		
 	}
 
 	@Override
 	public double nextDouble() {
-		return rand.nextDouble();
 	}
 
 	@Override
+	/**
+	 * Returns true if randomly generated number is even.
+	 */
 	public boolean nextBoolean() {
-		return rand.nextBoolean();
+		long x = RNG();
+		if (x % 2 = 0)
+			return true;
+		else return false;
+		
 	}
 
 	@Override
+	//returns random number between 0 and 99.
 	public int nextInt() {
-		return rand.nextInt();
+		long x = RNG();
+		return (int) x % 100;
 	}
 
+	//Will do later.
 	@Override
 	public int nextInt(int n) {
 		return rand.nextInt(n);
