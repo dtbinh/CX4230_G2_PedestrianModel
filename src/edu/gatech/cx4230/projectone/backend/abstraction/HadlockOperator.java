b12ad2f89c287684b473cbd53aa5dcb087243c6a
mcdc09 <ThomasBowling@Comcast.net>
2014-02-11 14:59:08 -0500

    Very-basic outline of a Hadlock Algorithm class

---
+++ src/edu/gatech/cx4230/projectone/backend/abstraction/HadlockOperator.java
@@ -0,0 +1,16 @@
+package edu.gatech.cx4230.projectone.backend.abstraction;
+
+/**
+ * Performs Hadlock's Algorithm on the cell array
+ * @author tbowling3
+ *
+ */
+public class HadlockOperator {
+	private CellManager cm;
+	
+	
+	public HadlockOperator(CellManager cake) {
+		this.cm = cake;
+	}
+
+}
