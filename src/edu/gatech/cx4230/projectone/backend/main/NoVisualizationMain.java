4bec3d1da9c20bdaa5fba7fc5acb16f119e5868e
mcdc09 <ThomasBowling@Comcast.net>
2014-02-25 17:44:57 -0500

    Updated NoVisualizationMain to process the results of a simulation

--- src/edu/gatech/cx4230/projectone/backend/main/NoVisualizationMain.java
+++ src/edu/gatech/cx4230/projectone/backend/main/NoVisualizationMain.java
@@ -1,14 +1,63 @@
 package edu.gatech.cx4230.projectone.backend.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.gatech.cx4230.projectone.backend.utilities.StatsHelper;
+
 /**
  * Class to create and run the experiments
  * @author tbowling3
  *
  */
 public class NoVisualizationMain {
+	private List<EndSimulationResult> results;
+	
+	public NoVisualizationMain(int trials) {
+		results = new ArrayList<EndSimulationResult>();
+		
+		for(int i = 0; i < trials; i++) {
+			run();
+		}
+	}
+	
+	public void run() {
+		PedestrianSimulation ps = new PedestrianSimulation();
+		
+		if(!ps.continueSim()) {
+			results.add(ps.getEndSimulationResult());
+		} else {
+			System.err.println("Simulation still running");
+		}
+	}
+	
+	public List<EndSimulationResult> getResults() {
+		return results;
+	}
+	
+	public void processResults() {
+		if(results != null && !results.isEmpty()) {
+			int trials = results.size();
+			int[] scores = new int[trials];
+			
+			for(int i = 0; i < trials; i++) {
+				scores[i] = results.get(i).getScore();
+			}
+			
+			double scoreAverage = StatsHelper.findAverage(scores);
+			double scoreSSD = StatsHelper.findStandardDeviation(scores);
+			
+			System.out.println("Trials run: " + trials);
+			System.out.println("Average Score: " + scoreAverage);
+			System.out.println("Sample Standard Deviation Score: " + scoreSSD);
+		}
+	}
 	
 	
 	public static void main(String[] args) {
-		PedestrianSimulation ps = new PedestrianSimulation();
+		int trials = 100;
+		NoVisualizationMain experiment = new NoVisualizationMain(trials);
+		experiment.processResults();
+		
 	}
 }
