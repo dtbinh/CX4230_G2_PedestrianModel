870fc4aaf4eec97ce652885801b2eae188c4dfe5
Elliott, Tucker Sterling <tselliott@gatech.edu>
2014-02-27 17:33:23 -0500

    Normal Distribution RNG
    Implemented a normal distribution RNG.

--- src/edu/gatech/cx4230/projectone/backend/random/CustomRNG.java
+++ src/edu/gatech/cx4230/projectone/backend/random/CustomRNG.java
@@ -11,7 +11,10 @@ public class CustomRNG extends AbstractRNG {
 		
 	}
 	
-	private long RNG() {
+	/**
+	 * Generates random seed based on system time. Variant of xorshift.
+	 */
+	private double RNG() {
 		long a = System.nanoTime() % 100000;
 		long b = System.nanoTime() % 200000;
 		long c = System.nanoTime() % 400000;
@@ -25,13 +28,17 @@ public class CustomRNG extends AbstractRNG {
 		return d;
 	}
 
-	@Override
+	/**
+	 * Returns random integer within range of possible doubles.
+	 */
 	public double nextDouble() {
 		long x = RNG();
-		return (double) x % Double.MAX_VALUE;
+		return x % Double.MAX_VALUE;
 	}
 
-	@Override
+	/**
+	 * Returns true if random seed is even, else false.
+	 */
 	public boolean nextBoolean() {
 		long x = RNG();
 		if (x % 2 == 0)
@@ -39,16 +46,37 @@ public class CustomRNG extends AbstractRNG {
 		else return false;
 	}
 
-	@Override
+	/**
+	 * Returns random integer within range of integers.
+	 */
 	public int nextInt() {
 		long x = RNG();
 		return (int) x % 2147483647;
 	}
 
-	@Override
+	/**
+	 * Returns random integer between 0 (inclusive) and n (exclusive).
+	 */
 	public int nextInt(int n) {
 		long x = RNG();
 		return (int) x % n;
+	}
+	
+	/**
+	 * Returns member of normal distribution of specified mean and standard
+	 * deviation. Based on Box-Muller transform.
+	 */
+	public double nextInNormal(double mean, double sd) {
+		double u = 2 * ((RNG() % 100000) / 100000) - 1;
+		double v = 2 * ((RNG() % 100000) / 100000) - 1;
+		double r = u * u + v * v;
+		
+		//reject
+		if (r == 0 || r > 1) return nextInNormal(mean, sd);
+		
+		//accept
+		double c = Math.sqrt(-2 * Math.log(r)/r);
+		return u * c;
 	}
 
 }
