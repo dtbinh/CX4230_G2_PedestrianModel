290a9407edf67935d5aa8acd4f5f0812b57158a0
Elliott, Tucker Sterling <tselliott@gatech.edu>
2014-02-27 17:42:51 -0500

    Fixed Normal RNG

--- src/edu/gatech/cx4230/projectone/backend/random/CustomRNG.java
+++ src/edu/gatech/cx4230/projectone/backend/random/CustomRNG.java
@@ -15,10 +15,10 @@ public class CustomRNG extends AbstractRNG {
 	 * Generates random seed based on system time. Variant of xorshift.
 	 */
 	private double RNG() {
-		long a = System.nanoTime() % 100000;
-		long b = System.nanoTime() % 200000;
-		long c = System.nanoTime() % 400000;
-		long d = System.nanoTime() % 800000;
+		long a = System.nanoTime() % 1000000;
+		long b = System.nanoTime() % 2000000;
+		long c = System.nanoTime() % 4000000;
+		long d = System.nanoTime() % 8000000;
 		
 		long t = a ^ (a << 11);
 		a = b;
@@ -32,7 +32,7 @@ public class CustomRNG extends AbstractRNG {
 	 * Returns random integer within range of possible doubles.
 	 */
 	public double nextDouble() {
-		long x = RNG();
+		double x = RNG();
 		return x % Double.MAX_VALUE;
 	}
 
@@ -40,7 +40,7 @@ public class CustomRNG extends AbstractRNG {
 	 * Returns true if random seed is even, else false.
 	 */
 	public boolean nextBoolean() {
-		long x = RNG();
+		double x = RNG();
 		if (x % 2 == 0)
 			return true;
 		else return false;
@@ -50,7 +50,7 @@ public class CustomRNG extends AbstractRNG {
 	 * Returns random integer within range of integers.
 	 */
 	public int nextInt() {
-		long x = RNG();
+		double x = RNG();
 		return (int) x % 2147483647;
 	}
 
@@ -58,7 +58,7 @@ public class CustomRNG extends AbstractRNG {
 	 * Returns random integer between 0 (inclusive) and n (exclusive).
 	 */
 	public int nextInt(int n) {
-		long x = RNG();
+		double x = RNG();
 		return (int) x % n;
 	}
 	
@@ -76,7 +76,7 @@ public class CustomRNG extends AbstractRNG {
 		
 		//accept
 		double c = Math.sqrt(-2 * Math.log(r)/r);
-		return u * c;
+		return mean + (u * c) * sd;
 	}
 
 }
