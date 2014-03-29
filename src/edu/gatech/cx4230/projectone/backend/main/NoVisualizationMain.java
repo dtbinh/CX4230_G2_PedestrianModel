480753333792a98efaa0ade9112188b6740577b9
mcdc09 <ThomasBowling@Comcast.net>
2014-03-29 18:37:09 -0400

    Removed comment

--- src/edu/gatech/cx4230/projectone/backend/main/NoVisualizationMain.java
+++ src/edu/gatech/cx4230/projectone/backend/main/NoVisualizationMain.java
@@ -48,7 +48,6 @@ public class NoVisualizationMain {
 			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
